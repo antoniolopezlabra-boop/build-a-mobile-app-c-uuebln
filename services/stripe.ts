27e83e418204e24956cdd4f718db955a7bb02d2e
antoniolopezlabra-boop <antonio.lopez.labra@hotmail.com>
2026-03-09 00:37:32 -0600

    feat: stripe service con payment links

---
+++ services/stripe.ts
@@ -0,0 +1,12 @@
+export const STRIPE_PUBLISHABLE_KEY = 'pk_test_51T8wtAJH1Ir1ABIzRNM9oIotWpI7IEg1joxVlEBGDiYfSBh2hdz2mcP7an6krNEfAcsyTwuflXgiEZqPneajUI3n00euzQjpN9';
+
+export const PLAN_PRICES = {
+    basico: {
+        priceId: 'price_basico',
+        link: 'https://buy.stripe.com/test_28EfZi6DH5eA0w16Zz7ok00',
+    },
+    premium: {
+        priceId: 'price_premium',
+        link: 'https://buy.stripe.com/test_3cIcN65zD36semR1Ff7ok01',
+    },
+};
